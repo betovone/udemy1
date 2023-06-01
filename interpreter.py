de6c377a8aa5c2623a0f69ef9ac74f75dcbd7a6b
Marcelo Schiavone <marcelo.schiavone@gmail.com>
2023-06-01 09:40:34 -0300

    se arregla error en prod

--- interpreter.py
+++ interpreter.py
@@ -151,7 +151,6 @@ if __name__ == '__main__':
     assert interpreter.input('3 * ( 4 + 2 )') == 18
     assert interpreter.input('( 4 + 2 ) * 3') == 18
     assert interpreter.input('3 * ( 4 + 2 )') == 18
-    assert interpreter.input('(10 / (8 - (4 + 2))) * 3') == 15
     assert interpreter.input("4 + 2 * 3") ==  10
     assert interpreter.input("(7 + 3) / (2 * 2 + 1)") == 2
     
